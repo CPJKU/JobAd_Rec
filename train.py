f28ac9ea5342ea812abd6a4ae6e32f9c7581a44b
Deepak Kumar <deepak@rechenknecht8.cp.jku.at>
2024-01-25 01:28:03 +0100

    update CreateData.py

--- train.py
+++ train.py
@@ -21,19 +21,19 @@ def main():
     parser.add_argument("--gpu_id", type=int, default=0, help="gpu no.")
     parser.add_argument("--seed", type=int, default=0, help="torch random seed")
     parser.add_argument("--model", type=str, default="bert--uncased", help="model name")
-    parser.add_argument("--balance", type=str, default="original_gender", help="model name")
-    parser.add_argument("--anonymous", default=False, action='store_true', help="remove gender from text input")
+    #parser.add_argument("--balance", type=str, default="balanced", help="the gender distribution in training data is skewed towards")
+    parser.add_argument("--anonymous", default=False, action='store_true', help="remove gender from candidate text")
     base_args, _ = parser.parse_known_args()
 
     model_name = base_args.model
-    balance = base_args.balance
+    #balance = base_args.balance
     if base_args.anonymous:
         condition = "UNK"
     else:
         condition = "KNO"  
     #train_batch_size = 64
     num_epochs = 4
-    model_save_path = '/home/deepak/RecSys2023/Dataset/BM25/'+balance+'/results/CE'+condition+'-'+model_name+'-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    model_save_path = '/home/deepak/JobAd_Rec/Models/'+condition+'_'+model_name+'_'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
     #pos_neg_ration = 4
     torch.manual_seed(base_args.seed)
@@ -41,49 +41,52 @@ def main():
 
     device = torch.device(f"cuda:{int(base_args.gpu_id)}")
 
-    if balance =='original_gender':
-        with open( '/share/hel/datasets/jobiqo/talent.com/UK_IR_subset/anonymous_new_bios.pkl', 'rb') as file:
+    try:
+        with open( '/share/hel/datasets/jobiqo/talent.com/JobRec/uk_jobs.pkl', 'rb') as file:
             dicts = pickle.load(file)
-    elif balance == 'female':
-        with open( '/home/deepak/RecSys2023/Dataset/new_bios_female.pkl', 'rb') as file:
-            dicts = pickle.load(file)
-    elif balance == 'male':
-        with open( '/home/deepak/RecSys2023/Dataset/new_bios_male.pkl', 'rb') as file:
-            dicts = pickle.load(file)
-    else:
-        print('No candidate dataset')
-    bios = pd.DataFrame(dicts).reset_index()
-    bios = bios.drop('index', axis=1)
+    except:
+        print('No Job Ad dataset')
+    uk_jobs = pd.DataFrame(dicts).reset_index()
+    uk_jobs = uk_jobs.drop('index', axis=1)
+
+
+    with open( '/share/hel/datasets/jobiqo/talent.com/JobRec/train.pkl', 'rb') as file:
+        dicts = pickle.load(file)
+    bios_train = pd.DataFrame(dicts)
+
+    with open( '/share/hel/datasets/jobiqo/talent.com/JobRec/val.pkl', 'rb') as file:
+        dicts = pickle.load(file)
+    bios_val = pd.DataFrame(dicts)
+
 
     gender_words = ['mr','his','he','him','himself','mrs','hers','she','her','herself','Alice', 'Bob']
     if base_args.anonymous:
         anonymous_bios = []
-        for i in range(len(bios['bio'])):
-            bio_string = bios['bio'][i]
+        for i in range(len(bios_train['bio'])):
+            bio_string = bios_train['bio'][i]
             for i in range(len(gender_words)):
                 bio_string = re.sub(gender_words[i], '_',bio_string,flags=re.I)
             anonymous_bios.append(bio_string)
 
-        bios['bio'] == anonymous_bios
+        bios_train['bio'] == anonymous_bios
+        anonymous_bios = []
+        for i in range(len(bios_val['bio'])):
+            bio_string = bios_val['bio'][i]
+            for i in range(len(gender_words)):
+                bio_string = re.sub(gender_words[i], '_',bio_string,flags=re.I)
+            anonymous_bios.append(bio_string)
+
+        bios_val['bio'] == anonymous_bios
 
 
-
-    with open( '/share/hel/datasets/jobiqo/talent.com/UK_IR_subset/train.pkl', 'rb') as file:
-        dicts = pickle.load(file)
-    uk_jobs_train = pd.DataFrame(dicts)
-
-    with open( '/share/hel/datasets/jobiqo/talent.com/UK_IR_subset/val.pkl', 'rb') as file:
-        dicts = pickle.load(file)
-    uk_jobs_val = pd.DataFrame(dicts)
-
-    val_hits = pd.read_csv('/home/deepak/RecSys2023/Dataset/BM25/'+balance+'/val_hits.txt', sep=' ', header=None, names=['query_id', 'Q0', 'doc_id', 'rank', 'score','Anserini'])
-    train_hits = pd.read_csv('/home/deepak/RecSys2023/Dataset/BM25/'+balance+'/train_hits.txt', sep=' ', header=None, names=['query_id', 'Q0', 'doc_id', 'rank', 'score','Anserini'])
+    val_hits = pd.read_csv('/share/hel/datasets/jobiqo/talent.com/JobRec/BM25/val_hits.txt', sep=' ', header=None, names=['query_id', 'Q0', 'doc_id', 'rank', 'score','Anserini'])
+    train_hits = pd.read_csv('/share/hel/datasets/jobiqo/talent.com/JobRec/BM25/train_hits.txt', sep=' ', header=None, names=['query_id', 'Q0', 'doc_id', 'rank', 'score','Anserini'])
     val_hits = val_hits.drop(['Q0','rank','score','Anserini'], axis='columns')
     val_hits['doc_id'] = val_hits['doc_id'].replace('doc','',regex=True).astype(int)
     train_hits = train_hits.drop(['Q0','rank','score','Anserini'], axis='columns')
     train_hits['doc_id'] = train_hits['doc_id'].replace('doc','',regex=True).astype(int)
 
-    model = CrossEncoder(model_name, num_labels=1, max_length=512, device=device)
+    model = CrossEncoder(model_name, num_labels=1, device=device)
 
     #In Train and Test for every datapoint with label 1 we add 4 negative samples
     import random
