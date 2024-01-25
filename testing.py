12e47eb03b66e19eed720072babf07c23291949a
ShawMask <shawmas2@gmail.com>
2024-01-25 16:30:12 +0100

    train.py: - added "--local" the option to run the code locally - added "--debias" argument to select debiasing method ("None" default, "reg" regularization and "adv" adversarial) - changed .fit debias argument from binary to string - code is functional with Default values on server

--- testing.py
+++ testing.py
@@ -5,19 +5,19 @@ import torch
 from tqdm.autonotebook import tqdm
 import numpy as np
 
-def testing(path,device):
+def testing(path,device,pth):
 
-    with open( '/share/hel/datasets/jobiqo/talent.com/JobRec/uk_jobs.pkl', 'rb') as file:
+    with open(f'{pth}share/hel/datasets/jobiqo/talent.com/JobRec/uk_jobs.pkl', 'rb') as file:
         dicts = pickle.load(file)
     uk_jobs = pd.DataFrame(dicts).reset_index()
     uk_jobs = uk_jobs.drop('index', axis=1)
 
 
-    with open( '/share/hel/datasets/jobiqo/talent.com/JobRec/test.pkl', 'rb') as file:
+    with open( f'{pth}share/hel/datasets/jobiqo/talent.com/JobRec/test.pkl', 'rb') as file:
         dicts = pickle.load(file)
     bios_test = pd.DataFrame(dicts)
 
-    test_hits = pd.read_csv('/share/hel/datasets/jobiqo/talent.com/JobRec/BM25/test_hits.txt', sep=' ', header=None, names=['query_id', 'Q0', 'doc_id', 'rank', 'score','Anserini'])
+    test_hits = pd.read_csv(f'{pth}share/hel/datasets/jobiqo/talent.com/JobRec/BM25/test_hits.txt', sep=' ', header=None, names=['query_id', 'Q0', 'doc_id', 'rank', 'score','Anserini'])
     test_hits = test_hits.drop(['Q0','rank','score','Anserini'], axis='columns')
     test_hits['doc_id'] = test_hits['doc_id'].replace('doc','',regex=True).astype(int)
 
@@ -32,5 +32,5 @@ def testing(path,device):
         cross_scores = model.predict(sentence_combinations)
         result.append({'corpus_id': new_uk_jobs.index[np.flip(np.argsort(cross_scores))],'scores':sorted(cross_scores, reverse=True)})
 
-    with open(path+'result.pkl', 'wb') as f:
+    with open(path+'shahed_result.pkl', 'wb') as f:
         pickle.dump(result, f)
--- train.py
+++ train.py
@@ -22,7 +22,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--gpu_id", type=int, default=0, help="gpu no.")
     parser.add_argument("--seed", type=int, default=0, help="torch random seed")
-    parser.add_argument("--model", type=str, default="bert--uncased", help="model name")
+    parser.add_argument("--model", type=str, default="bert-base-uncased", help="model name")
+    parser.add_argument("--local", default=False, action='store_true', help="changes path suitable to run locally")
+    parser.add_argument("--debias", type=str, default=None, help="select from debias methods "
+                                                                 "'reg' for regularization  "
+                                                                 "'adv' for adversarial")
     #parser.add_argument("--balance", type=str, default="balanced", help="the gender distribution in training data is skewed towards")
     #parser.add_argument("--anonymous", default=False, action='store_true', help="remove gender from candidate text")
     base_args, _ = parser.parse_known_args()
@@ -34,8 +38,9 @@ def main():
     #else:
     #    condition = "KNO"  
     #train_batch_size = 64
+    pth = "/home/shahed/" if base_args.local==True else "/"
     num_epochs = 4
-    model_save_path = '/home/deepak/JobAd_Rec/Models/'+str(base_args.seed)+'_'+model_name+'_'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    model_save_path = f'./Models/'+str(base_args.seed)+'_'+model_name+'_'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
     #pos_neg_ration = 4
     print(f"manual_seed({base_args.seed})")
@@ -46,10 +51,10 @@ def main():
     torch.backends.cudnn.deterministic = True
 
 
-    with open( '/share/hel/datasets/jobiqo/talent.com/JobRec/train_samples.pkl', 'rb') as file:
+    with open( f'{pth}share/hel/datasets/jobiqo/talent.com/JobRec/train_samples.pkl', 'rb') as file:
         train_samples = pickle.load(file)
 
-    with open( '/share/hel/datasets/jobiqo/talent.com/JobRec/dev_samples.pkl', 'rb') as file:
+    with open( f'{pth}share/hel/datasets/jobiqo/talent.com/JobRec/dev_samples.pkl', 'rb') as file:
         dev_samples = pickle.load(file)
 
     model = CrossEncoder(model_name, num_labels=1, device=device)
@@ -75,11 +80,11 @@ def main():
             warmup_steps=warmup_steps,
             output_path=model_save_path,
             save_best_model= True,
-            debias=False, #remove this if using orininal sentence-transformer libaray
+            debias=base_args.debias, #remove this if using orininal sentence-transformer libaray
             use_amp=True)
 
     #Test latest model
-    testing(path=model_save_path,device=device)
+    testing(path=model_save_path,device=device, pth=pth)
 
 if __name__ == "__main__":
 
